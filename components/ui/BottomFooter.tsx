d05286e954fa831a9a4931af2f3989ec71e2adee
aquasr9@gmail.com <aquasr9@gmail.com>
2024-09-02 18:02:41 +0530

    github link add

--- components/ui/BottomFooter.tsx
+++ components/ui/BottomFooter.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Separator } from "./separator";
 import { site_metadata } from "@/config/site_metadata";
+import { Icons } from "../icons/icons";
 
 interface IProps {
   className?: string;
@@ -66,22 +67,7 @@ export function BottomFooter({
             href="https://github.com/srx9"
             rel="noreferrer"
           >
-            <svg
-              className="h-8 w-8"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15.2 8.80005C16.4731 8.80005 17.694 9.30576 18.5941 10.2059C19.4943 11.1061 20 12.327 20 13.6V19.2H16.8V13.6C16.8 13.1757 16.6315 12.7687 16.3314 12.4687C16.0313 12.1686 15.6244 12 15.2 12C14.7757 12 14.3687 12.1686 14.0687 12.4687C13.7686 12.7687 13.6 13.1757 13.6 13.6V19.2H10.4V13.6C10.4 12.327 10.9057 11.1061 11.8059 10.2059C12.7061 9.30576 13.927 8.80005 15.2 8.80005Z"
-                fill="currentColor"
-              />
-              <path d="M7.2 9.6001H4V19.2001H7.2V9.6001Z" fill="currentColor" />
-              <path
-                d="M5.6 7.2C6.48366 7.2 7.2 6.48366 7.2 5.6C7.2 4.71634 6.48366 4 5.6 4C4.71634 4 4 4.71634 4 5.6C4 6.48366 4.71634 7.2 5.6 7.2Z"
-                fill="currentColor"
-              />
-            </svg>
+            <Icons.gitHub className="size-6" />
           </a>
         </div>
         <p className="mt-3 text-xs leading-7 text-gray-500  ">
